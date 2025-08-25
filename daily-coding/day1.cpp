106e90ffc3a9f93759b3d853048a2d956a8f393b
Supreet K <supreet3092004@gmail.com>
2025-08-25 20:45:33 +0530

    day 01

---
+++ daily-coding/day1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int a, b;
+ 
+ cout << "Enter the a value : ";
+ cin  >> a;
+ cout << "Enter the b value: ";
+ cin >> b;
+ cout << "The sum of and b is :" << (a+b) << endl;
+ return 0;
+}
