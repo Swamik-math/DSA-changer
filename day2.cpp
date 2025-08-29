9acde053d0e8a5d85215ba75381f21a5b6d9dd3c
Supreet K <supreet3092004@gmail.com>
2025-08-29 16:32:26 +0530

    code 1

---
+++ day2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 45;
+
+    if(n >= 0){
+        cout << "n is positive integer.";
+    }
+    else{
+        cout << "n is a negative integer.";
+    }
+
+    return 0;
+
+}
