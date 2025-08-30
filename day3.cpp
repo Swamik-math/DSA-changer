75d97c18fafd8de09ed1ed6f34b351d90909333b
Supreet K <supreet3092004@gmail.com>
2025-08-30 18:59:15 +0530

    while and for loop

--- day3.cpp
+++ day3.cpp
@@ -44,19 +44,38 @@ using namespace std;
 // } 
 
 
+// int main(){
+
+//     int n , sum = 0;
+//     cout << "Enter the n value : ";
+//     cin >> n;
+
+//     for( int i=0; i<=n; i++){
+//         sum += i;
+//         if(i == 10){
+//             break;
+//         }
+//     }
+//     cout << "The sum of "<< n<<" numbers is: "<< sum << endl;
+
+//     return 0;
+// }
+
+
 int main(){
-
-    int n , sum = 0;
-    cout << "Enter the n value : ";
+    int n ;
+    cout << "Enter the n value: ";
     cin >> n;
+    int sum =0;
 
-    for( int i=0; i<=n; i++){
-        sum += i;
-        if(i == 10){
-            break;
+    for(int i =1; i<=n; i++){
+        if( i%2 != 0){
+            sum += i;
+            cout << i << " ";
         }
+        cout << endl;
     }
-    cout << "The sum of "<< n<<" numbers is: "<< sum << endl;
-
+    
+    cout << "sum= " << sum << endl;
     return 0;
 }
