cfb583dde8de90245b183ee859c353cb1888b0a1
Supreet K <supreet3092004@gmail.com>
2025-08-30 15:45:36 +0530

    while and for loop

--- day3.cpp
+++ day3.cpp
@@ -1,23 +1,62 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout << "Enter the value of n: ";
+//     cin >> n;
+//     cout << endl;
+
+//     int count = 1;
+
+//     while(count <= n){
+//         cout << count << endl;
+//         count++;
+//     }
+
+//     cout << endl;
+//     return 0;
+
+// }
+
+// camel case
+
+
+
+
+// for loop  for(initialization; condition ; updastion)
+
 #include<iostream>
 using namespace std;
 
+// int main()
+// {
+
+
+//     int n;
+
+//     for(int i=0; i<=10; i=i+2){
+//         cout << i << " ";
+//     }
+//     cout << endl;
+    
+//     return 0;
+// } 
+
+
 int main(){
-    int n;
-    cout << "Enter the value of n: ";
+
+    int n , sum = 0;
+    cout << "Enter the n value : ";
     cin >> n;
-    cout << endl;
 
-    int count = 1;
-
-    while(count <= n){
-        cout << count << endl;
-        count++;
+    for( int i=0; i<=n; i++){
+        sum += i;
+        if(i == 10){
+            break;
+        }
     }
+    cout << "The sum of "<< n<<" numbers is: "<< sum << endl;
 
-    cout << endl;
     return 0;
-
 }
-
-// camel case
-// 
