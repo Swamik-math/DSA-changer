83cc45ff43e07a26ed587568bbac80304a1b94df
Supreet K <supreet3092004@gmail.com>
2025-08-30 14:51:22 +0530

    while and for loop

---
+++ while.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    cout << endl;
+
+    int count = 1;
+
+    while(count <= n){
+        cout << count << endl;
+        count++;
+    }
+
+    cout << endl;
+    return 0;
+
+}
+
+// camel case
+// 
