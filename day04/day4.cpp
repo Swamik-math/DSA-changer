441af34919d945af48e4a0e70fdd2d3040bb0823
Supreet K <supreet3092004@gmail.com>
2025-08-31 16:30:23 +0530

    day 04

--- day04/day4.cpp
+++ day04/day4.cpp
@@ -1,18 +1,67 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+// int main(){
 
+//     int n;
+//     cout << "Enter the n-value: ";
+//     cin >> n;
+
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             cout << "*" << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+
+// }
+
+
+// int main(){
+
+//     int n;
+//     cout << "Enter the n-value: ";
+//     cin >> n;
+
+//     for(int i=0; i<n; i++){
+//         char ch = 'A';
+//         for(int j=0; j<n; j++){
+//             cout << ch << " ";
+//             ch = ch+1;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+int main(){
     int n;
-    cout << "Enter the n-value: ";
+    cout << "Enter the n value : ";
     cin >> n;
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            cout << "*" << " ";
+    // int num =1;
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+
+    char ch = 'A';
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
     return 0;
 
 }
+
