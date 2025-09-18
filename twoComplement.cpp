387802d676e6aed332439ad1bc8e58265caf2023
Supreet K <supreet3092004@gmail.com>
2025-09-18 21:22:25 +0530

    two's complement

---
+++ twoComplement.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    
+
+}
+
+// two's complement is used to represent the negative decimal numbers in the binary number
+/*
+1. binary form convert
+2. prefix with 0
+3. 1's complement
+      0 -> 1
+      1 --> 0
+4. add 1
+*/
