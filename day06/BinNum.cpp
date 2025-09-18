b9a4c333b54d42cddd247fb9133f4a1647135a38
Supreet K <supreet3092004@gmail.com>
2025-09-18 19:47:26 +0530

    dec to binary

---
+++ day06/BinNum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int decToBinary(int decNum){
+    int ans = 0, pow = 1;
+
+    while(decNum > 0){
+        int rem = decNum % 2;
+        decNum /= 2;
+
+        ans += (rem * pow);
+        pow *= 10;
+    }
+    return ans;
+}
+
+int main(){
+    
+    int decNum = 50;
+    cout << decToBinary(decNum) << endl;
+
+    cout << endl << endl;
+
+    for(int i=0; i<= 20; i++){
+        cout << decToBinary(i) << endl;
+    }
+
+    return 0;
+    
+}
