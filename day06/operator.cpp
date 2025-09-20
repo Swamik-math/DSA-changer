d4a8961fe57a796218a941ad783318d7d1c1cf49
Supreet K <supreet3092004@gmail.com>
2025-09-20 12:04:34 +0530

    data modifiers

--- day06/operator.cpp
+++ day06/operator.cpp
@@ -1,1 +1,13 @@
 #include<iostream>
+using namespace std;
+
+int main(){
+
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(short int) << endl; // decreases the size
+    cout << sizeof(signed int) << endl; // it differ the number +ve and -ve
+    cout << sizeof(long long int) << endl;
+
+    return 0;
+}
