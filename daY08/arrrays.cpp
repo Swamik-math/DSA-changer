61ebdb16eabfee647ca0e55f4f39f7dabb7c94c1
Supreet K <supreet3092004@gmail.com>
2025-09-21 10:27:22 +0530

    array start

---
+++ daY08/arrrays.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    // int marks[5] = { 10, 12, 9 , 11, 32};
+
+    // // marks[1] = 99;
+    // // cout << marks[1] << endl;
+    // int n = 5;
+    // for(int i=0; i<5; i++){
+    //     cout << marks[i] << endl;
+    // }
+
+    int size = 10;
+    // int marks[size];
+    vector<int> marks(size);
+
+    for(int i=0; i<size; i++){
+        cin >> marks[i] ;
+    }
+    
+    return 0;
+}
+
+
+
+/* ARRAYS
+- in array we can only store same type of datatypes
+- are contiguous in memory
+- are linear
+
+1. int marks[100];
+2. int marks[5] = {1,2,3,4,5}; 
+3. double prize[];
+*/
