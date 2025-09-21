83431250d46f3e796b64f11537be5d11e3673f4c
Supreet K <supreet3092004@gmail.com>
2025-09-21 12:12:10 +0530

    linear search

--- daY08/linearSearch.cpp
+++ daY08/linearSearch.cpp
@@ -1,2 +1,23 @@
 #include<iostream>
 using namespace std;
+
+int linearSearch(int arr[], int sze, int target){
+    for(int i=0; i<sze; i++){
+        if(arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+
+    int arr[] = {21, 23, 45, 64, 11, 56};
+    int sze = 6;
+    int target = 11;
+    linearSearch(arr, sze, target);
+    cout << "The required element is in the position: " << linearSearch(arr, sze, target) << endl;
+
+    return 0;
+
+}
