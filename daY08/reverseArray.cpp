f09c823fe2bb45376355ed6bb74e8bcf8c472063
Supreet K <supreet3092004@gmail.com>
2025-09-22 21:53:13 +0530

    reverse Array

--- daY08/reverseArray.cpp
+++ daY08/reverseArray.cpp
@@ -3,21 +3,37 @@ using namespace std;
 
 // 2 pointer approach - to reverse an array
 
+void reverseArray(int arr[], int sz)
+{
+    int start = 0, end = sz-1;
+
+    while(start < end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
 int main(){
 
     int arr[] = {4,2,7,8,1,2,5};
-    int siz = 7;
+    int sz = 7;
 
-    for(int i=0; i<7; i++){
-        int start = arr[i];
-        for(int j=siz; j>0; j--){
-            int final = arr[j];
-            swap(start, final);
+    // for(int i=0; i<7; i++){
+    //     int start = arr[i];
+    //     for(int j=siz; j>0; j--){
+    //         int final = arr[j];
+    //         swap(start, final);
             
-        }
+    //     }
         
-    }
-    for(int i=0; i<siz; i++){
+    // }
+    // for(int i=0; i<siz; i++){
+    //     cout << arr[i] << " ";
+    // }
+    reverseArray(arr, sz);
+
+    for(int i=0; i<sz; i++){
         cout << arr[i] << " ";
     }
     cout << endl;
