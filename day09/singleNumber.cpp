69a8c2aa4aa4a80e611fb96e9f1158223de6c23e
Supreet K <supreet3092004@gmail.com>
2025-09-23 16:35:24 +0530

    single number

--- day09/singleNumber.cpp
+++ day09/singleNumber.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 using namespace std;
 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        int ans = 0;
+
+        for(int val : nums){
+            ans ^= val;
+        }
+    return ans;
+    }
+};
+
 int main(){
 
-    
+
 
 }
