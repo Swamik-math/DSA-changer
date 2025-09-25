ca401c7a72ce8bc4ee97b6bf7d7a1607753064bf
Supreet K <supreet3092004@gmail.com>
2025-09-25 18:04:34 +0530

    kadanes algo

--- day10/kadanes_Algo.cpp
+++ day10/kadanes_Algo.cpp
@@ -1,2 +1,27 @@
 //kadanesn algorithm 
-// 
+//most optimised
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+int main(){
+
+    int n = 5;
+    int arr[5] = {-1,2,-3,4,5};
+
+    int currSum = 0, maxSum = INT_MIN;
+
+    for(int i=0; i<n; i++){
+        currSum += arr[i];
+        maxSum = max(currSum, maxSum);
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+    cout << maxSum << endl;
+    return 0;
+}
+
+// time complexity = O(n)
+ 
