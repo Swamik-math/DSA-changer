9ce6b512cc34734a3c83aee296ccf95a725aef2b
Supreet K <supreet3092004@gmail.com>
2025-09-25 18:17:51 +0530

    pair sum

---
+++ day10/11_pairSum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n = 5;
+    int arr[5]  = {1,2,3,4,5};
+    int target = 7;
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+           
+            if(arr[i] + arr[j] == target){
+                cout <<"index " <<  i << " " << j << "-";
+                cout <<"nums " <<  arr[i] << " " << arr[j] << endl;
+            } 
+            
+        }
+    }
+    return 0;
+}
