7f0ca019e99746b9278964d6b40792974fe4d5cb
Supreet K <supreet3092004@gmail.com>
2025-09-26 17:44:31 +0530

    pair sum OPTIMISED METHOD

---
+++ day12/majorityElement.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    int num = 8;
+    int arr[] = {1,2,1,1,3,2,5,7};
+
+    //  int majorityElement(vector<int>& nums) {
+    //     int n = nums.size();
+
+    //     // sort 
+    //     sort(nums.begin(), nums.end());
+
+    //     //freq count
+
+    //     int freq =1, ans = nums[0];
+    //     for(int i=1; i<n; i++){
+    //         if(nums[i] == nums[i-1]){
+    //             freq++;
+    //         } else{
+    //             ans;
+    // }    freq = 1;
+    //             ans = nums[i];
+    //         }
+    //         if(freq > n/2){
+    //             return ans;
+    //         }
+    //     }
+        
+    //     return ans;
+
+
+    
+
+
+
+}
+
+
+//  sort(nums.begin(), nums.end());
+// it is used for sorting the array elements.
