39c05c792398caeb3391c24381465e2ea2cada71
Supreet K <supreet3092004@gmail.com>
2025-09-28 14:11:45 +0530

    time complexity 1

--- day13/timeComp.cpp
+++ day13/timeComp.cpp
@@ -12,5 +12,12 @@ O(n) = linear , n-factorial , kadanes algorithm , nth fibonacci dp
 
 O(n^2) , O(n^3)  - commmon sorting algorithm like bubble sorting
 
-O(log n ) - binary search
+O(log n ) - binary search  (optimised time complexity)
+
+O(n log n) - merg sort , quick sort, greedy algorithm
+
+O(2^n) - exponintial time complexities ( worst time complexities)
+
+O(n!) - n queens , knight tours
+    |- more worse then O(2^n)
 */
