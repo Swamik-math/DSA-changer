a057a467e4774c5e72e6789b7d380cd422743715
Supreet K <supreet3092004@gmail.com>
2025-09-28 14:04:53 +0530

    time complexity

---
+++ day13/timeComp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+}
+
+/*
+O(1) = constant time complexity
+
+O(n) = linear , n-factorial , kadanes algorithm , nth fibonacci dp
+
+O(n^2) , O(n^3)  - commmon sorting algorithm like bubble sorting
+
+O(log n ) - binary search
+*/
