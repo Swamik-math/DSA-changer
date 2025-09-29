c7aa6efa3bb5cba1b33e6e42356a731f0ade3299
Supreet K <supreet3092004@gmail.com>
2025-09-29 15:33:21 +0530

    time complexity

--- day14/14corg.cpp
+++ day14/14corg.cpp
@@ -8,13 +8,19 @@ int factorial(int n){
     return n * factorial(n-1);
 }
 
-
+int fib(int n){
+    if(n == 0 || n == 1){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
 
 int main(){
 
     int n = 10;
 
     cout << factorial(n) << endl;
+    cout << "Fibonacci of 10 is = " <<  fib(10) << endl; 
     return 0;
 }
 
@@ -25,4 +31,6 @@ int main(){
 
 1. f(n) = k + f(n-1)
    f(n-1) = k + f(n-2) 
+
+   for fibonacci series time complexity is  =  O(2^n)
 */
