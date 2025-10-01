e8462f720c8175af9cb0fc83415db5a078b756e8
Supreet K <supreet3092004@gmail.com>
2025-10-01 20:52:35 +0530

    dsa concept

---
+++ day14/arrayConcpt.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    
+}
+
+/*
+Time Complexity and Big O Notation
+Time Complexity is a measure of the amount of time 
+taken by an algorithm to run as a function of the length of the input. 
+Since the actual time depends on the hardware, programming language, etc.,
+ we instead count the number of elementary operations (like comparisons, assignments, or arithmetic operations).
+
+Big O Notation is the mathematical notation used to describe the upper bound (the worst-case scenario) 
+of an algorithm's running time. It focuses only on the dominant term and ignores constants, 
+as we're interested in the rate of growth.
+*/
