b4af229a295a7d7893f41d0be861f26af5787d42
Supreet K <supreet3092004@gmail.com>
2025-10-03 09:55:08 +0530

    stock buy and sell

--- day15/stockBuySell.cpp
+++ day15/stockBuySell.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
 
+int maxprofit(int price[], int n){
+    int maxP = 0, bestBuy = price[0];
 
+    for(int i=0; i<n; i++){
+        if(price[i] > bestBuy){
+            maxP = max(maxP, price[i] - bestBuy);
+        }
+        bestBuy = min(bestBuy, price[i]);
+    }
+    return maxP;
+}
 
 int main(){
-int maxP = 0, bestBuy = 
+
+    int price[] = {1,3,6,3,7,8,9};
+    int n = 7;
+    
+    cout << "Max profit : " << maxprofit(price, n) << endl;
+
+    return 0;
 
 }
