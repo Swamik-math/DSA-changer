068761816c837b1827c2ed037a14b3eb73425764
Supreet K <supreet3092004@gmail.com>
2025-10-08 21:07:35 +0530

    linked_list

--- day18/linked_List.cpp
+++ day18/linked_List.cpp
@@ -8,6 +8,23 @@ class Node:
     def_init_(self, data):
         self.data = data    (store value)
         self.next = None    (store address of the next node)
-        
+
+TYPES OF LINKED LIST:
+    single linked list
+    doubly linked list
+    circular linked list
+
+ADVANTAGES:
+dynamic memory allocation
+easy insert and deletion
+
+
+-> Common Interview Questions
+Reverse a linked list
+Detect a loop in a linked list (Floyd’s Cycle Detection)
+Find the middle element
+Merge two sorted linked lists
+Remove duplicates
+Delete nth node from end
 
 */
