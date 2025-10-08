a495b643f4e292c549aa1d2cbad5accf98f3580c
Supreet K <supreet3092004@gmail.com>
2025-10-08 21:02:47 +0530

    linked_list

---
+++ day18/linked_List.cpp
@@ -0,0 +1,13 @@
+/*
+
+linked list is a linear DAS structure in which elements are connected using pointers.
+
+pointer -> reference to the next node.
+
+class Node:
+    def_init_(self, data):
+        self.data = data    (store value)
+        self.next = None    (store address of the next node)
+        
+
+*/
