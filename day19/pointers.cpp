cca24d445b9ba1a16541288380158ce524b7e003
Supreet K <supreet3092004@gmail.com>
2025-10-11 21:18:28 +0530

    pointers concept

---
+++ day19/pointers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // to print the address of the stored value
+    int a = 10;
+    cout << &a << endl;
+
+    float m  = 10.354;
+    float* ptr2 = &m;
+    cout << &m << "    " << &ptr2 << endl;
+
+    // pointer to pointer
+
+    int b = 10;
+    int* ptr = &b;
+    int** parptr = &ptr;
+    cout << &ptr << "  " << &parptr << endl;
+
+    return 0;
+}
+
+// pointers - are the special variable that store the address of the variable
