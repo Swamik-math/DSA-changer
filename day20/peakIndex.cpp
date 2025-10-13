490de163333425b5fea614ee5d266d5c6cb389ce
Supreet K <supreet3092004@gmail.com>
2025-10-13 19:00:51 +0530

    peak index in mountain array problem

---
+++ day20/peakIndex.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int peakMountain(vector<int>& arr){
+    int n = arr.size();
+    for(int i=0; i< n-1; i++){
+        if(arr[i] > arr[i+1]){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4,6,9,3,2,1};
+    cout << "Peak index is: " << peakMountain(arr) << endl;
+    return 0;
+}
