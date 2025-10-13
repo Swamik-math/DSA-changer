a7dcca8b863e98785540d00883acaf58950f060a
Supreet K <supreet3092004@gmail.com>
2025-10-13 18:18:03 +0530

    search in the rotated search algorithm

---
+++ day20/rotatedSearch.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& A, int tar) {
+      int st = 0, end = A.size()-1;
+
+      while(st <= end){
+        int mid = st + (end - st)/2;
+        if(A[mid] == tar){
+            return mid;
+        }
+
+        if(A[st] <= A[mid]){
+            if(A[st] <= tar && tar <= A[mid]){
+                end = mid-1;
+            } else {
+                st = mid + 1;
+            } 
+            }else {
+                if(A[mid] <= tar && tar <= A[end]){
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+      }  
+    
+};
+
+int main(){
+
+    vector<int> arr = {4,5,6,7,0,1,2};
+    int target = 0;
+
+    Solution sol;
+    int idx = sol.search(arr, target);
+
+    if
+
+}
