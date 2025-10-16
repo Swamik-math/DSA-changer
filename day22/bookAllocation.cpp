db7e4c4e81adfa2804f4762b599820054960dfae
Supreet K <supreet3092004@gmail.com>
2025-10-16 18:16:53 +0530

    book allocation problem

---
+++ day22/bookAllocation.cpp
@@ -0,0 +1,17 @@
+// Book allocation problem
+/*
+You have to allocate books to M number of students so that the maxium number of pages allocated to a student is minimum
+
+> Each book should be allocated to a student.
+> Each student has to be allocated at least one book.
+> Allotment should be in contiguous manner/ order.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    
+
+}
