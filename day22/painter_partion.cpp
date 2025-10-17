b44d9c404ec5005c41b22c26bedf6d136bb94fee
Supreet K <supreet3092004@gmail.com>
2025-10-17 18:27:02 +0530

    painters partion problem

--- day22/painter_partion.cpp
+++ day22/painter_partion.cpp
@@ -1,2 +1,16 @@
 /* PAINTERS PARTION PROBLEM
 */
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int minTimeToPaint(vector<int> &arr, int n, int m){
+    int sum = 0, maxVal = INT_MIN;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+        maxVal = max(maxVal, arr[i]);
+    }
+
+}
