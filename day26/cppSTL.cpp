d6cc718ca71392083a67d69e7c2898b83709e13f
Supreet K <supreet3092004@gmail.com>
2025-11-06 22:13:53 +0530

    cpp stl library

--- day26/cppSTL.cpp
+++ day26/cppSTL.cpp
@@ -11,22 +11,29 @@ using namespace std;
 
 int main(){
 
-    vector<int> vec;
+    vector<int> vec = {1,2,3,4,5,6,7,8,9};
 
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(4);
-    vec.push_back(5);
-    vec.emplace_back(7);
+    // vec.push_back(1);
+    // vec.push_back(2);
+    // vec.push_back(3);
+    // vec.push_back(4);
+    // vec.push_back(5);
+    // vec.emplace_back(7);
+
+    vec.pop_back();
 
     for(int val : vec){
         cout << val << " ";
     }
     cout << endl;
 
-    cout << vec.size() << endl;
-    cout << vec.capacity() << endl;
+    // cout << vec.size() << endl;
+    // cout << vec.capacity() << endl;
+
+    // cout << "val at indx " << vec[2] << " or " << vec.at(2) << endl;
+    
+    cout << "front " << vec.front() << endl;
+    cout << "back " << vec.back() << endl;
 
     return 0;
 }
