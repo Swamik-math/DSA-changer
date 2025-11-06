71849cde2849d50f556280654b462ed86e3c22d3
Supreet K <supreet3092004@gmail.com>
2025-11-06 21:40:18 +0530

    cpp stl library

---
+++ day26/cppSTL.cpp
@@ -0,0 +1,19 @@
+/*
+
+> In array the size of the array is fixed.
+> We can only put up to a limit in the array.
+
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> vec;
+
+    cout << vec.size() << endl;
+
+    return 0;
+}
