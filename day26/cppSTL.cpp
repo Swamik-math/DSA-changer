aeed0d0878b7f67e6ee5599279fbc8ef3e4e514a
Supreet K <supreet3092004@gmail.com>
2025-11-06 21:51:00 +0530

    cpp stl library

--- day26/cppSTL.cpp
+++ day26/cppSTL.cpp
@@ -13,7 +13,20 @@ int main(){
 
     vector<int> vec;
 
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    vec.emplace_back(7);
+
+    for(int val : vec){
+        cout << val << " ";
+    }
+    cout << endl;
+
     cout << vec.size() << endl;
+    cout << vec.capacity() << endl;
 
     return 0;
 }
