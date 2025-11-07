1d1e4120066f24f9d6ff816a52107c397dafd604
Supreet K <supreet3092004@gmail.com>
2025-11-07 19:23:35 +0530

    cpp stl library

--- day26/SLTcppp.cpp
+++ day26/SLTcppp.cpp
@@ -2,14 +2,43 @@
 #include<vector>
 using namespace std;
 
-int main(){
-    vector<int> vec = {1,2,3,4,5,6,7,8,9,0};
+// int main(){
+//     vector<int> vec = {1,2,3,4,5,6,7,8,9,0};
     
-    vec.erase(vec.begin()+1, vec.begin()+3);
+//     vec.erase(vec.begin()+1, vec.begin()+3);
+//     vec.insert(vec.begin() +2, 100);
 
-    for(int val : vec){
-        cout << val << " ";
+//     for(int val : vec){
+//         cout << val << " ";
+//     }
+//     cout << endl;
+
+//    cout << "is empty : " << vec.empty() << endl;
+
+//     return 0;
+// }
+
+// clear
+// empty
+// erase
+// insert
+
+int main(){
+
+    vector<int> vec = {1,2,3,4,5,6,7};
+
+    cout << "vec.begin : " << *(vec.begin()) << endl;
+    cout << "vec.end : " << *(vec.end()) << endl;
+
+    vector<int>::iterator it;
+    for(it = vec.begin(); it != vec.end(); it++){
+        cout << *(it) << endl;
     }
-    cout << endl;
+
+    vector<int>::reverse_iterator it;
+    for(auto it = vec.rbegin(); it != vec.rend(); it++){
+        cout << *(it) << endl;
+    }
+
     return 0;
 }
