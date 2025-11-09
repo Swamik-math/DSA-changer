4176f7838edf9ff6ada95fac8666ab0bba67b22a
Supreet K <supreet3092004@gmail.com>
2025-11-09 14:02:06 +0530

    add stack operations demo (day 28)

---
+++ day_28/stackOp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+
+
+
+}
+
+/*
+STACK- 
+    A stack is a datastructure thet folllow LIFO.
+
+    st.push()
+    st.top()
+    st.pop()
+    st.empty()
+    
+*/
