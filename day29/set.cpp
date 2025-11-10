3ffc8b21daac396eece7b28cbd56df1638283491
Supreet K <supreet3092004@gmail.com>
2025-11-10 21:06:30 +0530

    multi_set and unordered_set in dsa concept

--- day29/set.cpp
+++ day29/set.cpp
@@ -3,13 +3,22 @@
 using namespace std;
 
 int main(){
-    set<int> s;
+    multiset<int> s;
 
     s.insert(1);
     s.insert(11);
     s.insert(7);
     s.insert(4);
     s.insert(3);
+    s.insert(3);
+    
+    s.insert(4);
+    s.insert(3);
+    s.insert(3);
+
+    cout << s.size() << endl;
+    cout << "lower_bound = " << *(s.lower_bound(4)) << endl;
+    cout << "upper_bound = " << *(s.upper_bound(4)) << endl;
 
     for(auto val : s){
         cout << val << " ";
@@ -17,3 +26,20 @@ int main(){
     cout << endl;
     return 0;
 }
+
+/*
+insert, emplace
+count
+erase
+find
+size, empty, erase
+
+O(log n) = time complexity
+
+> lower_bound() - the value should not be less than the key value
+> upper_bound() - the value should be greater than key
+
+1. multiset<int>
+2. unordered_set<int>
+
+*/
