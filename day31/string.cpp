a64a8dec657006a5e68931300bc547b9ab7f6cd6
Supreet K <supreet3092004@gmail.com>
2025-11-13 17:47:53 +0530

    learnt string in cpp

---
+++ day31/string.cpp
@@ -0,0 +1,36 @@
+/*
+> In  programin if any word is written in " " - then it is called as string.
+> char arrys - 
+
+'\0' = null character in string.
+
+> to print entire line - cin.getline( str, len, delim?)
+     delim - delimiter
+
+*/
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    // char str[] = {'a', 'b', 'c', 'd', 'e', '\0'};
+    // char ni[] = "monorola";
+    // cout << str << endl;
+    // cout << strlen(str) << endl;
+    // cout << strlen(ni) << endl;
+
+    char str[12];
+    
+    cout << "Enter the string: ";
+    cin.getline(str, 12);
+    // cout << "output : " << str << endl;
+
+    for(char ch : str){
+        cout << ch << " ";
+
+    }
+    cout << endl;
+
+    return 0;
+}
