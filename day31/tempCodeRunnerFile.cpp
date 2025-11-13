401c297dcfa5a7bf13dc31f6552d62217084a9f8
Supreet K <supreet3092004@gmail.com>
2025-11-13 18:22:01 +0530

    learnt string in cpp

---
+++ day31/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+ring s1  = "I ";
+    string s2 = "love ";
+    string s3 = "India !";
+
+    string s4 = s1 + s2 + s3;
+
+    cout << s4 << endl;
+    cout << (s1 == s2) << endl;
+    cout << "s3 string length : " << s3.length() << endl;
+    cout << "\n\n" << endl;
