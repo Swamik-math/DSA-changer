a275accb6fb95641aecbee124d80d2322a268d10
Supreet K <supreet3092004@gmail.com>
2025-11-13 18:46:43 +0530

    learnt string in cpp

--- day31/algo_str.cpp
+++ day31/algo_str.cpp
@@ -2,30 +2,31 @@
 #include<string>
 using namespace std;
 
-int main(){
-    // string str = "apana college";
-    // str = "Supreet k";
+// int main(){
+//     // string str = "apana college";
+//     // str = "Supreet k";
 
-    // // string is dynamic in nature - means it can be changed in the runtime.(runtime resize)
+//     // // string is dynamic in nature - means it can be changed in the runtime.(runtime resize)
 
-    // cout << str << endl;
+//     // cout << str << endl;
 
-    string s1  = "I ";
-    string s2 = "love ";
-    string s3 = "India !";
+//     string s1  = "I ";
+//     string s2 = "love ";
+//     string s3 = "India !";
 
-    string s4 = s1 + s2 + s3;
+//     string s4 = s1 + s2 + s3;
 
-    cout << s4 << endl;
-    cout << (s1 == s2) << endl;
-    cout << "s3 string length : " << s3.length() << endl;
-    cout << "\n\n" << endl;
+//     cout << s4 << endl;
+//     cout << (s1 == s2) << endl;
+//     cout << "s3 string length : " << s3.length() << endl;
+//     cout << "\n\n" << endl;
 
-    string s;
-    getline(cin, s);
-    cout << "out_put : " << s << endl;
+//     string s;
+//     getline(cin, s);
+//     cout << "out_put : " << s << endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
 // lexico-graphic : being in dictonary order.
+
