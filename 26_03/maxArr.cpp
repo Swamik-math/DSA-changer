333647f31d35b44070faebdc59ff1e9fdc1428e4
Supreet K <supreet3092004@gmail.com>
2026-01-30 18:50:37 +0530

    --

---
+++ 26_03/maxArr.cpp
@@ -0,0 +1,24 @@
+// KADENS ALGORITHM
+
+#include<climits>
+#include<iostream>
+using namespace std;
+
+// int maxSubArraySum(int arr[], int n){
+//     int currSum = 0;
+//     int maxSum = INT_MIN;
+
+//     for(int i=0; i<n; i++){
+//         currSum += arr[i];
+//         maxSum = max(maxSum, currSum);
+
+//         if(currSum <0)
+//             currSum = 0;
+//     }
+// }
+
+
+
+int main(){
+    
+}
