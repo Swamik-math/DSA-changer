9c0d9c58df7ade2c7043751ee23cb5b7b654f65e
Supreet K <supreet3092004@gmail.com>
2026-02-02 18:10:06 +0530

    vector

---
+++ 26_05/vector.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+VECTOR: 
+> dyanmic array
+> size can grow and shrink automatically
+> elements are stored in contigious memory
+> fast access like array
+
+
+ vector<int> v(5);
+ vector<int> v(5, 10);
+
+to push 
+
+v.push_back(10);
+v.push_back(43);
+v.push_back(31);
+
+v.at(0);
+v.front();
+v.back();
+
+v.pop_back();
+v.clear();
+
+v.erase(v.begin()+2);
+*/
