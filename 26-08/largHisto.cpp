2871be60369a024e6894d5c3024d6289919f2708
Supreet K <supreet3092004@gmail.com>
2026-02-05 21:51:10 +0530

    largest rectangele in hte histogram

--- 26-08/largHisto.cpp
+++ 26-08/largHisto.cpp
@@ -1,1 +1,34 @@
 #include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+int largeArea(vector<int>& heights){
+    int n = heights.size();
+    stack<int> st;
+    int maxArea = 0;
+
+    for(int i=0; i<=n; i++){
+        int currHeight;
+
+        if(i == n){
+            currHeight = 0;
+        }
+        else{
+            currHeight = heights[i];
+        }
+
+        while(!st.empty() && currHeight < heights[st.top()]){
+            int h = heights[st.top()];
+            st.pop();
+
+            int right = i;
+            int left = st.empty() ? -1 : st.top();
+            int width = right - left -1;
+
+            maxArea = max(maxArea, h*width);
+        }
+        st.push(i);
+    }
+    return maxArea;
+}
