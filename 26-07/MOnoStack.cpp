4d21077b6b06432698cc6994ed0971e055718458
Supreet K <supreet3092004@gmail.com>
2026-02-05 06:52:07 +0530

    monotonic elemants

--- 26-07/MOnoStack.cpp
+++ 26-07/MOnoStack.cpp
@@ -18,10 +18,42 @@ vector<int> nextGreater(vector<int>& arr){
     stack<int> st;  
 
     for(int i=n-1; i>=0 ; i--){
-        while(!st.empty() && st.top() >= arr[i])
-            st.top();
+        while(!st.empty() && st.top() <= arr[i])
+            st.pop();
 
         if(!st.empty())
             ans[i] = st.top();
+        
+        st.push(arr[i]);
     }
+    return ans;
+}
+
+// STOCK SPAN PROBLEM
+vector<int> stockSpan(vector<int>& price){
+    int n = price.size();
+    vector<int> span(n);
+    stack<int> st;
+
+    for(int i=0; i<n; i++){
+        while(!st.empty() && price[st.top()] <= price[i])
+            st.pop();
+
+        span[i] = st.empty() ? i+1 : i-st.top();
+        st.push(i);
+    }
+    return span;
+}
+
+int main(){
+
+    vector<int> arr = {4, 5, 2, 25};
+    vector<int> result = nextGreater(arr);
+
+    cout << "Next greater element :";
+    for(int num : result){
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
 }
